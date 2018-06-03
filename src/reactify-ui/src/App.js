6c835af4d8baf3a7e22ef9abefad455738fabfcd
CFE <hello@teamcfe.com>
2018-06-03 14:19:11 -0700

    8 - Hello World

--- src/reactify-ui/src/App.js
+++ src/reactify-ui/src/App.js
@@ -2,18 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Posts from './posts/Posts';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Posts />
     );
   }
 }
---
+++ src/reactify-ui/src/posts/Posts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Posts extends Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default Posts;
