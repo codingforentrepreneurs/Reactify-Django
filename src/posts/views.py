49fe7e358ef226ce32a0c8521fcb8e7334b346d1
CFE <hello@teamcfe.com>
2018-06-06 12:17:36 -0700

    28 - Django Rest Framework Custom Pagination

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,28 @@
-from rest_framework import generics, permissions
-
+from rest_framework import generics, permissions, pagination
+from rest_framework.response import Response
 
 from .models import Post
 from .permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer
+
+class PostPageNumberPagination(pagination.PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'size'
+    max_page_size = 20
+
+    def get_paginated_response(self, data):
+        author = False
+        user = self.request.user
+        if user.is_authenticated:
+            author = True
+        context = {
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'author': author,
+            'results': data,
+        }
+        return Response(context)
 
 
 
@@ -23,6 +42,7 @@ class PostListCreateAPIView(generics.ListCreateAPIView):
     queryset            = Post.objects.all()
     serializer_class    = PostSerializer
     permission_classes  = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class    = PostPageNumberPagination
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
